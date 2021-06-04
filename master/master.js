4a8bf2f4e067f9ab1a2be75b31c6ca30b04cc562
Ahmed Ashraf <ahmed.ashraf.cmp@gmail.com>
2021-06-05 00:33:44 +0200

    Assign tablets to servers

--- master/master.js
+++ master/master.js
@@ -1,20 +1,29 @@
 const mongoose = require("mongoose");
 const constants = require("../scripts/constants");
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const { Socket } = require("dgram");
+const io = new Server(server);
 
-const totalTablets = 85800;
+mongoose.connect(`${constants.connectionString}`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-function connectDB() {
-  mongoose.connect(`${constants.connectionString}`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  console.log("You'r connected!");
+});
 
-  const db = mongoose.connection;
-  db.on("error", console.error.bind(console, "connection error:"));
-  db.once("open", function () {
-    console.log("You'r connected!");
-  });
+server.listen(3000, () => {
+  console.log("Master: listening on *:3000");
+});
 
+function movieDB() {
   const moviesSchema = new mongoose.Schema({
     imdb_title_id: String,
     title: String,
@@ -41,11 +50,24 @@ function connectDB() {
   });
 
   const Movie = mongoose.model("IMDbMovies", moviesSchema);
-  //   db.imdbmovies.createIndex({ year: 1 }, { background: true });
   return Movie;
 }
 
-// Responsible for dividing data tables into tablets.
+function metadataDB() {
+  const metadataSchema = new mongoose.Schema({
+    firstServer: Object,
+    secondServer: Object,
+  });
+
+  const Metadata = mongoose.model("metadata", metadataSchema);
+  return Metadata;
+}
+
+/* 
+1. Responsible for dividing data tables into tablets.
+2. Responsible for assigning tablets to tablet servers 
+3. Metadata table indicating the row key range (start key-end key) for each tablet server.
+*/
 async function divideTables(Movie) {
   let docs = await Movie.find({}).sort({ year: 1 }).limit(30000);
 
@@ -60,14 +82,57 @@ async function divideTables(Movie) {
   return rangeKeys;
 }
 
-let Movie = connectDB();
-divideTables(Movie)
-  .then((rangeKeys) => {
-    console.log(rangeKeys);
-  })
-  .catch((err) => {
-    console.error(err);
-  });
+function asignServers() {
+  let Movie = movieDB();
+  divideTables(Movie)
+    .then((rangeKeys) => {
+      const metadata = {
+        firstServer: {
+          setA: {
+            startYear: rangeKeys[0].startYear,
+            endYear: rangeKeys[0].endYear,
+          },
+          setB: {
+            startYear: rangeKeys[1].startYear,
+            endYear: rangeKeys[1].endYear,
+          },
+          range: {
+            startYear: rangeKeys[0].startYear,
+            endYear: rangeKeys[1].endYear,
+          },
+        },
+        secondServer: {
+          setA: {
+            startYear: rangeKeys[2].startYear,
+            endYear: rangeKeys[2].endYear,
+          },
+          setB: {
+            startYear: rangeKeys[3].startYear,
+            endYear: rangeKeys[3].endYear,
+          },
+          range: {
+            startYear: rangeKeys[2].startYear,
+            endYear: rangeKeys[3].endYear,
+          },
+        },
+      };
+      metadataTable = metadataDB();
+      metadataTable.collection.insertOne(metadata, function (err, metadata) {
+        if (err) {
+          return console.error(err);
+        } else {
+          console.log("Metadata inserted successfully");
+        }
+      });
 
-// Responsible for assigning tablets to tablet servers
-// Metadata table indicating the row key range (start key-end key) for each tablet server.
+      io.on("connection", (socket) => {
+        socket.on("server connection", (msg) => {
+          socket.emit("metadata has written successfully", metadata);
+        });
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
+asignServers();
