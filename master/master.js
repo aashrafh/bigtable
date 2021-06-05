f918332da18c6ca60e10b9807448d3ab6d26bd27
Ahmed Ashraf <ahmed.ashraf.cmp@gmail.com>
2021-06-05 20:38:51 +0200

    Add master events

--- master/master.js
+++ master/master.js
@@ -3,11 +3,14 @@ const constants = require("../scripts/constants");
 const express = require("express");
 const app = express();
 const http = require("http");
-const server = http.createServer(app);
+const master = http.createServer(app);
 const { Server } = require("socket.io");
-const { Socket } = require("dgram");
-const MovieModel = require('../Movie_model');
-const io = new Server(server);
+const MovieModel = require("../Movie_model");
+const io = new Server(master);
+
+let metadata = {};
+let tablets = [];
+let tabletsCount = 30000;
 
 mongoose.connect(`${constants.connectionString}`, {
   useNewUrlParser: true,
@@ -20,10 +23,9 @@ db.once("open", function () {
   console.log("You'r connected!");
 });
 
-server.listen(3000, () => {
-  console.log("Master: listening on *:3000");
+master.listen(8080, () => {
+  console.log("Master: listening on *:8080");
 });
-
 
 function metadataDB() {
   const metadataSchema = new mongoose.Schema({
@@ -41,14 +43,14 @@ function metadataDB() {
 3. Metadata table indicating the row key range (start key-end key) for each tablet server.
 */
 async function divideTables(Movie) {
-  let docs = await Movie.find({}).sort({ year: 1 }).limit(30000);
+  tablets = await Movie.find({}).sort({ year: 1 }).limit(tabletsCount);
 
-  const tabletSize = Math.floor(docs.length / 4);
+  const tabletSize = Math.floor(tablets.length / 4);
   let rangeKeys = [];
   for (let i = 0; i < 4; i++) {
     rangeKeys.push({
-      startYear: docs[i * tabletSize]["year"],
-      endYear: docs[(i + 1) * tabletSize - 1]["year"],
+      startYear: tablets[i * tabletSize]["year"],
+      endYear: tablets[(i + 1) * tabletSize - 1]["year"],
     });
   }
   return rangeKeys;
@@ -58,7 +60,7 @@ function asignServers() {
   let Movie = MovieModel;
   divideTables(Movie)
     .then((rangeKeys) => {
-      const metadata = {
+      metadata = {
         firstServer: {
           setA: {
             startYear: rangeKeys[0].startYear,
@@ -96,15 +98,67 @@ function asignServers() {
           console.log("Metadata inserted successfully");
         }
       });
-
-      io.on("connection", (socket) => {
-        socket.on("server connection", (msg) => {
-          socket.emit("metadata has written successfully", metadata);
-        });
-      });
     })
     .catch((err) => {
       console.error(err);
     });
 }
 asignServers();
+
+let serverCount = 0;
+let servers = [
+  {
+    id: "",
+    tabletsNumber: Math.floor(tabletsCount / 2),
+  },
+  {
+    id: "",
+    tabletsNumber: Math.floor(tabletsCount / 2),
+  },
+];
+
+io.on("connection", (socket) => {
+  socket.on("get-meta", (msg) => {
+    socket.emit("send-meta", metadata);
+  });
+
+  socket.on("server-connect", (serverSocket) => {
+    servers[serverCount].id = socket.id;
+    socket.emit(
+      "send-data",
+      tablets.slice(
+        serverCount * Math.floor(tabletsCount / 2),
+        (serverCount + 1) * Math.floor(tabletsCount / 2)
+      )
+    );
+    serverCount++;
+  });
+
+  socket.on("confirm-load", (tabletsNumber) => {
+    if (servers[0].id == socket.id) {
+      servers[0].tabletsNumber = tabletsNumber;
+    } else {
+      servers[1].tabletsNumber = tabletsNumber;
+    }
+
+    if (!balanceLoad()) {
+      socket.emit("update-meta", metadata);
+      socket.emit("update-data");
+    }
+  });
+
+  socket.on("update-data", (socketServer) => {
+    if (servers[0].id == socket.id) {
+      socket.emit("new-data", tablets.slice(0, Math.floor(tabletsCount / 2)));
+    } else {
+      socket.emit(
+        "new-data",
+        tablets.slice(Math.floor(tabletsCount / 2), tabletsCount.legnth)
+      );
+    }
+  });
+});
+
+function balanceLoad() {
+  
+}
--- tabletServer.js
+++ tabletServer.js
@@ -1,106 +1,105 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const constants = require("./scripts/constants");
-const socketIO = require('socket.io');
-const http = require('http');
+const socketIO = require("socket.io");
+const http = require("http");
 const server = http.createServer(app);
 const io = socketIO(server);
-const mongoose = require('mongoose');
-const MovieModel = require('./Movie_model');
+const mongoose = require("mongoose");
+const MovieModel = require("./Movie_model");
+
+const ioClient = require("socket.io-client");
+const socketClient = ioClient("http://localhost:8080/", { reconnect: true });
 
 mongoose.connect(`${constants.connectionString}`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
+socketClient.on("connect", () => {
+  console.log("Connected with the master successfully");
+  socketClient.emit("")
+});
 
-  readRows = async (movie) => {
-    return await MovieModel.find({title : movie.title}).sort({year : 1});
-  }
+const readRows = async (movie) => {
+  return await MovieModel.find({ title: movie.title }).sort({ year: 1 });
+};
 
-  addRow = async (movie) => {
-      let movieToSave = new MovieModel(movie);
-      return await movieToSave.save();
-  }
-  
-  deleteRows = async (movie) => {
-    return await MovieModel.deleteOne({title : movie.title});
-  }
+const addRow = async (movie) => {
+  let movieToSave = new MovieModel(movie);
+  return await movieToSave.save();
+};
 
-  deleteCells = async (movie,cells) => {
-      return await MovieModel.updateOne(movie,{
-            $unset: cells
-        });
-  }
+const deleteRows = async (movie) => {
+  return await MovieModel.deleteOne({ title: movie.title });
+};
 
-  setRow = async (movie) => {
-    return await MovieModel.updateOne({ title: movie.title }, {
-            $set: movie
-        });
-  }
+const deleteCells = async (movie, cells) => {
+  return await MovieModel.updateOne(movie, {
+    $unset: cells,
+  });
+};
 
-io.on('connection',(socket) =>{
-    console.log('new user connected');
-     // listen for message from user
-    socket.on('Set', async (Movie)=>{
-        console.log(`set operation`);
-        // lock the server
-        const movie = await setRow(Movie);
+const setRow = async (movie) => {
+  return await MovieModel.updateOne(
+    { title: movie.title },
+    {
+      $set: movie,
+    }
+  );
+};
 
-        if (movie !== null)
-            socket.emit('successful',"Set Row");
-        else 
-            socket.emit('unsuccessful',"Set Row");
-        
-    });
-    
-    socket.on('AddRow', async (Movie)=>{
-        console.log(`add row operation`);
-        // connect to the db and add the row from it
-        const movie = await addRow(Movie);
-        if (movie !== null)
-            socket.emit('successful',"Add Row");
-        else 
-            socket.emit('unsuccessful',"Add Row");
-        
-    });
-    
-    socket.on('DeleteCells', async (Movie,Cells)=>{
-        console.log(`delete cells operation`);
-        // connect to the db and delete the cells from it
-        movie = await deleteCells(Movie,Cells);
-        if (movie !== null)
-            socket.emit('successful',"delete Cells");
-        else 
-            socket.emit('unsuccessful',"delete Cells");
-    });
-    
-    socket.on('DeleteRow', async (movie)=>{
-        console.log(`delete row operation`);
-        // connect to the db and delete the row from it
-        movie = await deleteRows(movie);
-        if (movie !== null)
-            socket.emit('successful',"delete Row");
-        else 
-            socket.emit('unsuccessful',"delete Row");
-    });
-         
-    socket.on('ReadRows', async (Message) => {
-        console.log(`read rows operation`);
-        // read the movie from server
-        let movies = await readRows(Message);
-        if (movies !== null)
-            //send it back to the client
-            socket.emit('read',movies);
-        else 
-            socket.emit('unsuccessful',"Read Rows");
-    })
-    
-     // when server disconnects from user
-     socket.on('disconnect', ()=>{
-       console.log('disconnected from user');
-     });
-})
+io.on("connection", (socket) => {
+  console.log("new user connected");
+  // listen for message from user
+  socket.on("Set", async (Movie) => {
+    console.log(`set operation`);
+    // lock the server
+    const movie = await setRow(Movie);
 
-port = 4000
-server.listen(port,() => `listening on port ${port}`);
+    if (movie !== null) socket.emit("successful", "Set Row");
+    else socket.emit("unsuccessful", "Set Row");
+  });
+
+  socket.on("AddRow", async (Movie) => {
+    console.log(`add row operation`);
+    // connect to the db and add the row from it
+    const movie = await addRow(Movie);
+    if (movie !== null) socket.emit("successful", "Add Row");
+    else socket.emit("unsuccessful", "Add Row");
+  });
+
+  socket.on("DeleteCells", async (Movie, Cells) => {
+    console.log(`delete cells operation`);
+    // connect to the db and delete the cells from it
+    movie = await deleteCells(Movie, Cells);
+    if (movie !== null) socket.emit("successful", "delete Cells");
+    else socket.emit("unsuccessful", "delete Cells");
+  });
+
+  socket.on("DeleteRow", async (movie) => {
+    console.log(`delete row operation`);
+    // connect to the db and delete the row from it
+    movie = await deleteRows(movie);
+    if (movie !== null) socket.emit("successful", "delete Row");
+    else socket.emit("unsuccessful", "delete Row");
+  });
+
+  socket.on("ReadRows", async (Message) => {
+    console.log(`read rows operation`);
+    // read the movie from server
+    let movies = await readRows(Message);
+    if (movies !== null)
+      //send it back to the client
+      socket.emit("read", movies);
+    else socket.emit("unsuccessful", "Read Rows");
+  });
+
+  // when server disconnects from user
+  socket.on("disconnect", () => {
+    console.log("disconnected from user");
+  });
+});
+
+port = 4000;
+server.listen(port, () => `listening on port ${port}`);
