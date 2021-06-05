0106e45860c309802832b4e1dd2161455daafeaf
AhmedSherif304 <ahmedsherifelminshawy@gmail.com>
2021-06-06 00:15:32 +0200

    adding master communication to the client

--- client.js
+++ client.js
@@ -1,23 +1,49 @@
 const io = require('socket.io-client')
 const socket = io('http://localhost:4000/',{reconnect : true});
+const socketMaster = io('http://localhost:8080/',{reconnect : true});
 const MovieModel = require('./Movie_model');
+
+
+//master-client operations
+
 let metaData = {}
+
+socketMaster.on('connect',() => {
+    console.log('connected to the master successfully');
+    socketMaster.emit('get-meta');
+})
+
+
+socketMaster.on('send-meta',(meta)=>{
+    metaData = meta;
+    console.log(meta);
+})
+
+socketMaster.on('update-meta',(meta)=>{
+    metaData = meta;
+    console.log(meta);
+})
+
+
+
+
+
+
+// server-client operations
 
 const operation = 'ReadRows'
 const movie = {title : 'Split'};
 const cells = {'year' : 0};
-// Add a connect listener
+
 socket.on('connect',() => { 
     console.log('Connected!');
 });
 
 
-socket.on('metaData',(metaData) => { // this for receiving the meta data from the master upon connection or upon update
-    metaData = this.metaData
-})
 
 //set of queries to be sent
-socket.emit(operation, movie,cells); // send a read request
+if (meta !== {})
+    socket.emit(operation, movie,cells); 
 
 socket.on('successful',(message)=>{
     console.log(`the ${message} operation has been done successfully`);
