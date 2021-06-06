f75ef96dbc78678da0c32a61679e8e7015b39bd4
Ahmed Ashraf <ahmed.ashraf.cmp@gmail.com>
2021-06-06 03:55:14 +0200

    Connect the server to master and client

--- master/master.js
+++ master/master.js
@@ -6,11 +6,13 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const master = http.createServer(app);
+const ioServer = require("socket.io");
 
 let metadata = {};
 let tablets = [];
 let servers = [];
-// let clients = [];
+let clients = [];
+let tabletServerCount = 0;
 
 mongoose.connect(`${constants.connectionString}`, {
   useNewUrlParser: true,
@@ -47,80 +49,137 @@ async function divideTables(Movie) {
   return rangeKeys;
 }
 
-// async function metaToClients(meta) {
-//   for (let i = 0; i < clients.length; i++) {
-//     clients[i].emit("send-meta", meta);
-//   }
-// }
+async function metaToClients(meta) {
+  for (let i = 0; i < clients.length; i++) {
+    clients[i].emit("send-meta", meta);
+  }
+}
 
 async function tabletsToServers(meta) {
   for (let i = 0; i < servers.length; i++) {
     servers[i].emit(
       "send-tablets",
-      tablets.slice(
-        i * Math.floor(tablets.length / 2),
-        (i + 1) * Math.floor(tablets.length / 2)
-      )
+      {
+        tablets: tablets.slice(
+          i * Math.floor(tablets.length / 2),
+          (i + 1) * Math.floor(tablets.length / 2)
+        ),
+        index = i
+      }
     );
     console.log("Emitting the tablets");
   }
 }
 
 function buildMeta(rangeKeys) {
-  return {
-    firstServer: {
-      setA: {
-        startYear: rangeKeys[0].startYear,
-        endYear: rangeKeys[0].endYear,
+  if (!tabletServerCount) return {};
+  else if (tabletServerCount === 1)
+    return {
+      firstServer: {
+        setA: {
+          startYear: rangeKeys[0].startYear,
+          endYear: rangeKeys[1].endYear,
+        },
+        setB: {
+          startYear: rangeKeys[2].startYear,
+          endYear: rangeKeys[3].endYear,
+        },
+        range: {
+          startYear: rangeKeys[0].startYear,
+          endYear: rangeKeys[3].endYear,
+        },
       },
-      setB: {
-        startYear: rangeKeys[1].startYear,
-        endYear: rangeKeys[1].endYear,
+    };
+  else
+    return {
+      firstServer: {
+        setA: {
+          startYear: rangeKeys[0].startYear,
+          endYear: rangeKeys[0].endYear,
+        },
+        setB: {
+          startYear: rangeKeys[1].startYear,
+          endYear: rangeKeys[1].endYear,
+        },
+        range: {
+          startYear: rangeKeys[0].startYear,
+          endYear: rangeKeys[1].endYear,
+        },
       },
-      range: {
-        startYear: rangeKeys[0].startYear,
-        endYear: rangeKeys[1].endYear,
+      secondServer: {
+        setA: {
+          startYear: rangeKeys[2].startYear,
+          endYear: rangeKeys[2].endYear,
+        },
+        setB: {
+          startYear: rangeKeys[3].startYear,
+          endYear: rangeKeys[3].endYear,
+        },
+        range: {
+          startYear: rangeKeys[2].startYear,
+          endYear: rangeKeys[3].endYear,
+        },
       },
-    },
-    secondServer: {
-      setA: {
-        startYear: rangeKeys[2].startYear,
-        endYear: rangeKeys[2].endYear,
-      },
-      setB: {
-        startYear: rangeKeys[3].startYear,
-        endYear: rangeKeys[3].endYear,
-      },
-      range: {
-        startYear: rangeKeys[2].startYear,
-        endYear: rangeKeys[3].endYear,
-      },
-    },
-  };
+    };
+}
+
+function send(meta) {
+  if (!tabletServerCount) metaToClients(meta);
+  else {
+    tabletsToServers(meta);
+    metaToClients(meta);
+  }
+}
+
+function processMeta(rangeKeys) {
+  metadata = buildMeta(rangeKeys);
+  metadataTable = metadataDB();
+  metadataTable.collection.insertOne(metadata, function (err, metadata) {
+    if (err) {
+      return console.error(err);
+    } else {
+      console.log("Metadata inserted successfully");
+    }
+  });
+}
+
+async function balanceLoad() {
+  const Movie = MovieModel;
+  divideTables(Movie).then((rangeKeys) => {
+    processMeta(rangeKeys);
+    send(metadata);
+  });
 }
 
 async function asignServers() {
   const Movie = MovieModel;
   divideTables(Movie)
     .then((rangeKeys) => {
-      metadata = buildMeta(rangeKeys);
-      metadataTable = metadataDB();
-      metadataTable.collection.insertOne(metadata, function (err, metadata) {
-        if (err) {
-          return console.error(err);
-        } else {
-          console.log("Metadata inserted successfully");
-        }
-      });
-      tabletsToServers(metadata);
+      processMeta(rangeKeys);
+      send(metadata);
 
-      const ioMaster = require("socket.io")(master);
+      const ioMaster = ioServer(master);
       ioMaster.on("connection", (socket) => {
         servers.push(socket);
-        console.log("A new server connected");
-        tabletsToServers(metadata);
+        console.log(
+          "Master: new server has attached with index: ",
+          tabletServerCount++
+        );
+
+        send(metadata);
+        balanceLoad();
+
+        socket.on("serverWrite", () => {
+          balanceLoad();
+        });
+
         socket.on("disconnect", () => {
-          // servers.pop(socket);
+          servers = servers.filter(
+            (tabletServer) => tabletServer.id !== socket.id
+          );
+          tabletServerCount--;
+          console.log("Master: a server has disconnected, re-balancing...");
+          balanceLoad();
         });
       });
     })
@@ -128,6 +187,7 @@ async function asignServers() {
       console.error(err);
     });
 }
+
 asignServers()
   .then((res) => {
     console.log("Assigned!!");
--- tablet/server.js
+++ tablet/server.js
@@ -1,14 +1,151 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const tabletServer = http.createServer(app);
+
+const mongoose = require("mongoose");
+const MovieModel = require("./Movie_model");
+const constants = require("./scripts/constants");
+
 const masterURL = "http://localhost:8080";
 const ioClient = require("socket.io-client");
+const ioServer = require("socket.io");
+
+let master = null;
+let tablets = [];
+let port = 3000;
+
+mongoose.connect(`${constants.connectionString}`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const readRows = async (movie) => {
+  return await MovieModel.find({ title: movie.title }).sort({ year: 1 });
+};
+
+const addRow = async (movie) => {
+  let movieToSave = new MovieModel(movie);
+  return await movieToSave.save();
+};
+
+const deleteRows = async (movie) => {
+  return await MovieModel.deleteOne({ title: movie.title });
+};
+
+const deleteCells = async (movie, cells) => {
+  return await MovieModel.updateOne(movie, {
+    $unset: cells,
+  });
+};
+
+const setRow = async (movie) => {
+  return await MovieModel.updateOne(
+    { title: movie.title },
+    {
+      $set: movie,
+    }
+  );
+};
+
+function connectToClient() {
+  tabletServer.listen(port, () => {
+    console.log("Tablet server has started for client connections");
+
+    const tabletSocket = ioServer(tabletServer);
+    tabletSocket.on("connection", (socket) => {
+      socket.on("Set", async (Movie) => {
+        console.log(`set operation`);
+        const movie = await setRow(Movie);
+        if (movie !== null) {
+          socket.emit("successful", "Set Rows");
+          socketClient.emit("operation", "successfully", "Set Cells", movie);
+        } else {
+          socket.emit("unsuccessful", "Set Rows");
+          socketClient.emit("operation", "unsuccessfully", "Set Cells", movie);
+        }
+      });
+
+      socket.on("AddRow", async (Movie) => {
+        console.log(`add row operation`);
+        const movie = await addRow(Movie);
+        if (movie !== null) {
+          socket.emit("successful", "Add Row");
+          socketClient.emit("operation", "successfully", "Add Row", movie);
+        } else {
+          socket.emit("unsuccessful", "Add Row");
+          socketClient.emit("operation", "unsuccessfully", "Add Row", movie);
+        }
+      });
+
+      socket.on("DeleteCells", async (Movie, Cells) => {
+        console.log(`delete cells operation`);
+        const movie = await deleteCells(Movie, Cells);
+        if (movie !== null) {
+          socket.emit("successful", "delete Cells");
+          socketClient.emit(
+            "operation",
+            "successfully",
+            "delete cells",
+            movie,
+            cells
+          );
+        } else {
+          socket.emit("unsuccessful", "delete Cells");
+          socketClient.emit(
+            "operation",
+            "unsuccessfully",
+            "delete Cells",
+            movie,
+            cells
+          );
+        }
+      });
+
+      socket.on("DeleteRow", async (Movie) => {
+        console.log(`delete row operation`);
+        const movie = await deleteRows(Movie);
+        if (movie !== null) {
+          socket.emit("successful", "delete Row");
+          socketClient.emit("operation", "successfully", "delete Row", movie);
+        } else {
+          socket.emit("unsuccessful", "delete Row");
+          socketClient.emit("operation", "unsuccessfully", "delete Row", movie);
+        }
+      });
+
+      socket.on("ReadRows", async (Movie) => {
+        console.log(`read rows operation`);
+        const movies = await readRows(Movie);
+        //const movie = serverTablets[serverTablets.findIndex((tablet) => { return tablet.title == Movie.title })];
+        if (movies !== null) {
+          socket.emit("successful", "Read Row");
+          socket.emit("read", movies);
+          socketClient.emit("operation", "successfully", "Read Row", movies);
+        } else {
+          socket.emit("unsuccessful", "Read Row");
+          socketClient.emit("operation", "unsuccessfully", "Read Row", movies);
+        }
+      });
+    });
+  });
+}
 
 function connectToMaster() {
   const masterSocket = ioClient(masterURL);
   masterSocket.on("connect", () => {
     console.log("Connected to the master");
-    masterSocket.on("send-tablets", (tablets) => {
-      console.log("Recieved tablets successfully");
+    masterSocket.on("send-tablets", (res) => {
+      tablets = res.tablets;
+      if (res.index == 1) port = 4000;
+      console.log("Recieved number of tablets = ", tablets.length);
+      connectToClient();
     });
   });
+  master = masterSocket;
 }
 
 connectToMaster();
+
+// 1- When to emit 'serverWrite' event ?
+// 2- We need to lock server operations
